72b49f6a49837864dac748f302c83664c3e7d439
steven cao <stevencao23@gmail.com>
2020-12-11 23:34:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,30 @@
 def addition(num1, num2)
-
+  add = num2 + num1
+  return add
+  add
 end
 
 def subtraction(num1, num2)
+  subtract = num2 - num1
 
 end
 
 def division(num1, num2)
+  divide = num2 / num1
 
 end
 
 def multiplication(num1, num2)
+  multiply = num1 * num2
 
 end
 
 def modulo(num1, num2)
+  modul = num2 % num1
 
 end
 
 def square_root(num)
+  num = Math.sqrt(#{num})
 
 end
---
+++ test.rb
@@ -0,0 +1,7 @@
+def addition(num1, num2)
+  return add = num2 + num1
+  puts add
+  add
+end
+
+addition 2, 3
